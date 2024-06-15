254681cd107014fa798629eaef817d7e00012460
dostoevsky <dostoevsky@kali.root>
2024-06-15 23:18:00 +0300

    add community

--- app/handlers/model.py
+++ app/handlers/model.py
@@ -1,10 +1,25 @@
+import asyncio
 from aiogram import Router
 from magic_filter import F
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, Message
 from app.callbacks_factory.base import CommunityCallbackData
 from app.broker import broker_send, Data, PhotoTopic
+from app.config import settings, bot
+from aiogram.methods import SendMediaGroup
+from app.types import Album
+from aiogram.types import CallbackQuery
+from app.callbacks_factory.base import CommunityCallbackData
+from app.s3.s3_client import S3Service
 
 model_router = Router()
+
+
+async def download_and_upload_media(media, file_type):
+    file_info = await bot.get_file(media.file_id)
+    filename = f"{media.file_id}.{file_type}"
+    downloaded_file = await bot.download_file(file_info.file_path)
+    uploaded_link = await S3Service.put_object(filename, downloaded_file)
+    return uploaded_link
 
 
 @model_router.callback_query(CommunityCallbackData.filter(F.action == "load_photo"))
@@ -14,3 +29,50 @@ async def load_photo(callback: CallbackQuery):
         Data(photo=PhotoTopic(photo=list("dsadsa"), user="dsasda"))
     )
     await callback.message.answer("Отправлено на оценку")
+
+
+@model_router.message(F.video)
+async def send_video(message: Message):
+    try:
+        if message.video:
+            await S3Service.get_s3_client()
+            link = await download_and_upload_media(message.video, "mp4")
+            await message.answer(link)
+    finally:
+        await S3Service.close_s3_session()
+
+
+@model_router.message(F.photo)
+async def send_photo(message: Message):
+    try:
+        if message.photo:
+            await S3Service.get_s3_client()
+            link = await download_and_upload_media(message.photo[-1], "jpg")
+            await message.answer(link)
+    finally:
+        await S3Service.close_s3_session()
+
+
+@model_router.message(F.media_group_id)
+async def send_post_to_channel(message: Message, album: Album):
+    message: SendMediaGroup = album.copy_to(chat_id=settings.CHANNEL_ID)
+    # TODO анонимно или нет message.from_user.username
+    # await bot.send_media_group(**dict(messag))
+
+    try:
+        await S3Service.get_s3_client()
+
+        tasks = []
+        if album.photo:
+            tasks.extend(
+                [download_and_upload_media(media, "jpg") for media in album.photo]
+            )
+        if album.video:
+            tasks.extend(
+                [download_and_upload_media(media, "mp4") for media in album.video]
+            )
+        links = await asyncio.gather(*tasks)
+        await message.reply("Сообщение отправлено в комьюнити!")
+        await message.answer("\n".join(links))
+    finally:
+        await S3Service.close_s3_session()
--- app/keyboards/user.py
+++ app/keyboards/user.py
@@ -3,21 +3,28 @@ from app.callbacks_factory.base import (
     UserCallbackData,
 )
 
+
 def get_user_inline_buttons() -> InlineKeyboardButton:
-    inline_kb_full = InlineKeyboardMarkup(row_width=2,inline_keyboard=[
-    [
-    InlineKeyboardButton(
-        text="Отправить Мастеру", callback_data=UserCallbackData(action="send_master").pack()
-    ),
-    ],
-    [InlineKeyboardButton(
-        text="Добавить мастера", callback_data=UserCallbackData(action="add_master").pack()
-    )],
-    [InlineKeyboardButton(
-        text="История", callback_data=UserCallbackData(action="history").pack()
-    )],
-    [InlineKeyboardButton(
-        text="Назад", callback_data=UserCallbackData(action="start").pack()
-    )]
-    ])
+    inline_kb_full = InlineKeyboardMarkup(
+        row_width=2,
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Cтатистика",
+                    callback_data=UserCallbackData(action="send_master").pack(),
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="История",
+                    callback_data=UserCallbackData(action="history").pack(),
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="Назад", callback_data=UserCallbackData(action="start").pack()
+                )
+            ],
+        ],
+    )
     return inline_kb_full
