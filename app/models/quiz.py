94189a58d067978ab3de8403083de16ec17a867b
Nikita Kotov <nik-kotov-98@inbox.ru>
2024-06-15 17:52:56 +0300

    added quiz table

---
+++ app/models/quiz.py
@@ -0,0 +1,13 @@
+import uuid
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db import Base
+
+
+class Quiz(Base):
+    __tablename__ = "quiz"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, index=True)
+    id_photo: Mapped[str] = mapped_column(nullable=False)
+    id_mark: Mapped[str] = mapped_column(nullable=False)
+    label: Mapped[int] = mapped_column(nullable=True)
